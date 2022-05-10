4831c878cabb60fdc6976c1587eb527acf4808d8
Anastasiia_Dremina <Anastasiia.Dremina@externals.adidas.com>
2022-05-10 20:00:34 +0300

    homework solution v1

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,25 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	sort.Sort(SortedByKeys(input))
+	for _, v := range input {
+		result = append(result, v)
+	}
+	return result
+}
+
+type SortedByKeys map[int]string
+
+func (input SortedByKeys) Len() int {
+	return len(input)
+}
+
+func (input SortedByKeys) Less(i, j int) bool {
+	return i < j
+}
+
+func (input SortedByKeys) Swap(i, j int) {
+	input[i], input[j] = input[j], input[i]
 }
